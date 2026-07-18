fea85af19ded2726fed90021301589d78239e8e5
arghie <arghiemarkroxas@gmail.com>
2026-07-18 12:09:35 +0800

    test commit

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -10,7 +10,7 @@ export default function Signup() {
             <h1>Signup</h1>
 
             <div>
-              <FormSignup className="w-full max-w-80" /> bbb
+              <FormSignup className="w-full max-w-80" />
             </div>
 
             <div className="mt-7 flex flex-col gap-3 text-center">
